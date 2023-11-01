b8b2571c1b3a05e98191dc789e29c6a2ef6988af
mtbrr26 <breiermathieu@gmail.com>
2023-11-01 20:09:25 +0100

    Rename python file to HW5

--- test/test__HW3.py
+++ test/test__HW3.py
@@ -7,7 +7,7 @@ import datetime
 import pytest
 import pytest_cov
 
-import HW3
+import test.HW5 as HW5
 
 # 1)
 # Create a function called "count_simba" that counts
@@ -24,7 +24,7 @@ class TestCountSimba(unittest.TestCase): #test class for count_simba function
             "simba and nala are lions.",
             "sIMba and Nala are lions."
         ]
-        count = HW3.count_simba(string_list)
+        count = HW5.count_simba(string_list)
         self.assertEqual(count, 3)  # "Simba" occurs 3 times in list in different cases so it passes the test
 
     def test__count_simba_no_occurrence(self): #test for no occurrence
@@ -32,7 +32,7 @@ class TestCountSimba(unittest.TestCase): #test class for count_simba function
             "Hakuna matata",
             "I laugh in the face of danger."
         ]
-        count = HW3.count_simba(string_list)
+        count = HW5.count_simba(string_list)
         self.assertEqual(count, 0)  # "Simba" does not occur in list so it passes the test
 
     def test__count_simba_multiple_occurrences(self): #test for multiple occurrences
@@ -40,7 +40,7 @@ class TestCountSimba(unittest.TestCase): #test class for count_simba function
             "SimbaSimbaSimbaSimba",
             "Simba Simba Simba"
         ]
-        count = HW3.count_simba(string_list)
+        count = HW5.count_simba(string_list)
         self.assertEqual(count, 7)  # "Simba" occurs 7 times in list so it passes the test
 
     def test__count_simba_mixed_strings(self):
@@ -51,7 +51,7 @@ class TestCountSimba(unittest.TestCase): #test class for count_simba function
             "Hakuna matata",
             "Timon, Pumba and Simba are friends, but Simba could eat the other two."
         ]
-        count = HW3.count_simba(string_list)
+        count = HW5.count_simba(string_list)
         self.assertEqual(count, 3)  # "Simba" occurs 3 times
 
 if __name__ == '__main__': #ensures that the test class is only run when the file is run directly
@@ -71,7 +71,7 @@ class TestGetDayMonthYear(unittest.TestCase): #Test class on the the get_day_mon
     def test__get_day_month_year_single_date(self):
         date_list = [date(2023, 2, 9)]
         expected_df = pd.DataFrame({'day': [9], 'month': [2], 'year': [2023]})
-        result_df = HW3.get_day_month_year(date_list)
+        result_df = HW5.get_day_month_year(date_list)
         self.assertTrue(expected_df.equals(result_df)) #only a single date so it passes the test 
 
     def test__get_day_month_year_multiple_dates(self): #test for multiple dates
@@ -81,7 +81,7 @@ class TestGetDayMonthYear(unittest.TestCase): #Test class on the the get_day_mon
             'month': [2, 12, 1],
             'year': [2023, 2022, 1997]
         })
-        result_df = HW3.get_day_month_year(date_list)
+        result_df = HW5.get_day_month_year(date_list)
         self.assertTrue(expected_df.equals(result_df)) #multiple dates so it passes the test
     
 if __name__ == '__main__':
@@ -101,13 +101,13 @@ class TestComputeDistance(unittest.TestCase): #test class for compute_distance f
     def test__compute_distance_single_pair(self): #test for a single pair
         coordinates = [((41.23, 23.5), (41.5, 23.4))]
         expected_distances = [31.132]  # Expected distance for the single pair
-        result_distances = HW3.compute_distance(coordinates)
+        result_distances = HW5.compute_distance(coordinates)
         self.assertEqual(result_distances, expected_distances) #only a single pair so it passes the test
 
     def test__compute_distance_multiple_pairs(self): #test for multiple pairs
         coordinates = [((41.23, 23.5), (41.5, 23.4)), ((52.38, 20.1), (52.3, 17.8))]
         expected_distances = [31.132, 157.006]  # Expected distances for the two pairs
-        result_distances = HW3.compute_distance(coordinates)
+        result_distances = HW5.compute_distance(coordinates)
         self.assertEqual(result_distances, expected_distances) #multiple pairs so it passes the test
 
 if __name__ == '__main__':
@@ -128,25 +128,25 @@ class TestSumGeneralIntList(unittest.TestCase): #test class for sum_general_int_
     def test__sum_general_int_list_single_list(self): #test for a single list
         lst = [1, 2, 3, 4]
         expected_result = 10
-        result = HW3.sum_general_int_list(lst)
+        result = HW5.sum_general_int_list(lst)
         self.assertEqual(result, expected_result) #only a single list so it passes the test
 
     def test__sum_general_int_list_nested_lists(self): #test for nested lists
         lst = [1, [2, [3, 4]], 5]
         expected_result = 15
-        result = HW3.sum_general_int_list(lst)
+        result = HW5.sum_general_int_list(lst)
         self.assertEqual(result, expected_result) #nested lists so it passes the test
 
     def test__sum_general_int_list_empty_list(self): #test for empty list
         lst = []
         expected_result = 0
-        result = HW3.sum_general_int_list(lst)
+        result = HW5.sum_general_int_list(lst)
         self.assertEqual(result, expected_result) #empty list so it passes the test
 
     def test__sum_general_int_list_no_integers(self): #test for no integers
         lst = ["a", [1.5, "b"], {"key": 2}]
         expected_result = 0
-        result = HW3.sum_general_int_list(lst)
+        result = HW5.sum_general_int_list(lst)
         self.assertEqual(result, expected_result) #no integers so it passes the test
 
 if __name__ == '__main__':
